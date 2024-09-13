7268ce4f348a1fcd8fd86d25c149a46c70682b9d
onkarabile masoko <onkarabilemasoko1@gmail.com>
2024-09-13 08:52:23 +0200

    Starting my project from scratch

--- onka-store/app/components/Pagination.js
+++
@@ -1,4 +0,0 @@
-<nav className="flex justify-between mt-4">
-  <Link href={`/?page=${parseInt(currentPage) - 1}`} className="px-4 py-2 bg-primary text-white">Previous</Link>
-  <Link href={`/?page=${parseInt(currentPage) + 1}`} className="px-4 py-2 bg-secondary text-white">Next</Link>
-</nav>
